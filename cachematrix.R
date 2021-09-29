a03be000547e4fb70a0314011e1ad7139da7183b
Vani-Vora <62203793+Vani-Vora@users.noreply.github.com>
2021-09-29 11:00:59 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,29 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL
-        set <- function(y) {
-                x <<- y
-                inv <<- NULL
+        inv <- NULL                                                     # no inverse is calculated yet
+        set <- function(y) {                                            # y must be of class matrix()
+                x <<- y                                                 # a new matrix is active now
+                inv <<- NULL                                            # the inverse hasn't been calculated yet
         }
-        get <- function() (x)
-        setInverse <- function(inverse) (inv <<- inverse)
-        getInverse <- function() (inv)
-        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+        get <- function() (x)                                           # returns the active matrix, 'x'
+        setInverse <- function(inverse) (inv <<- inverse)               # value passed from cacheSolve
+        getInverse <- function() (inv)                                  # returns the inverse value
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) # list of functions passed to cacheSolve
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        inv <- x$getInverse()
-        if (!is.null(inv)){
+        ## Return a matrix that is the inverse of 'x'                    
+        inv <- x$getInverse()                                           # initialize 'i' from arg func
+        if (!is.null(inv)){                                             # return inverse if it's available
                 message("Getting cached data")
                 return(inv)
         }
-        mat <- x$get()
-        inv <- solve(mat, ...)
-        x$setInverse(inv)
-        inv
+        mat <- x$get()                                                  # get data if 'i' hasn't been calculated
+        inv <- solve(mat, ...)                                          # calculate inverse of the passed matrix
+        x$setInverse(inv)                                               # set 'i' to calculated
+        inv                                                             # return answer 
 }
